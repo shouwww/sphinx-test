8b13ccdb3594550c1dabba375ec8676845ee6966
shouwww <uw0516sho@gmail.com>
2023-02-18 12:07:21 +0900

    test

--- files/conf.py
+++ files/conf.py
@@ -13,7 +13,7 @@ author = 'ShoheiKobata'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
